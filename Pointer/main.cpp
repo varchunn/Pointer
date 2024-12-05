af6305c0f7570846574e801183d82b870b4278c1
varchunn <varvaram186@gmail.com>
2024-12-05 02:28:00 +0200

    Initial Commit

---
+++ Pointer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Pointer
+//
+//  Created by Варя Мінєнкова on 05.12.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
